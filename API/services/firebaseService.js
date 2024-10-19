094eb20bf6e16f13924491ff60f55eb5e17fabaa
Bryan Indarto <bryan.firjatulloh@siemens.com>
2024-10-19 21:15:34 +0700

    added frontend backend and database connection

---
+++ API/services/firebaseService.js
@@ -0,0 +1,11 @@
+// Example Firebase service functions to interact with Firestore or Storage
+
+const uploadImageToFirebase = async (imageData) => {
+    // Logic to upload the image to Firebase
+    return 'Firebase URL for uploaded image';
+  };
+  
+  module.exports = {
+    uploadImageToFirebase,
+  };
+  
---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+import Dashboard from './components/Dashboard/Dashboard'
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
